65598e2b6a4ec65fbdb42610681c09f374d14dfd
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-03-29 10:34:33 +0000

    ADDED: cors, ratelimiter to app config

--- backend/src/config/setupApp.js
+++ backend/src/config/setupApp.js
@@ -4,15 +4,24 @@ import cors from 'cors';
 import express from 'express';
 import morgan from 'morgan';
 import httpStatusCode from 'http-status-codes';
+import { rateLimit } from 'express-rate-limit';
 import ApiRoutes from '../routes';
 import ApiError, { errorResponse } from '../middleware/error';
 
 const logger = morgan('combined');
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: 'draft-7',
+  legacyHeaders: false,
+});
+
 export default function setUpApp(app) {
-  app.use(express.json());
+  app.use(limiter);
+  app.options('*', cors());
   app.use(logger);
-  app.use(cors());
+  app.use(express.json());
   app.use('/api/v1', ApiRoutes.apiRoutes);
   app.use('/api/v1', ApiRoutes.authRoutes);
   app.use('/api/v1', ApiRoutes.userRoutes);
